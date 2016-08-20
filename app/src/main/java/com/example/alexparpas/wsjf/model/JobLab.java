767d8963bc502e73517ba9246fcf7cd9e564098e
alexparpas <alexandrosparpas@gmail.com>
2016-08-20 18:22:34 +0300

    The app is now able to read and write from the database

--- app/src/main/java/com/example/alexparpas/wsjf/model/JobLab.java
+++ app/src/main/java/com/example/alexparpas/wsjf/model/JobLab.java
@@ -1,16 +1,17 @@
 package com.example.alexparpas.wsjf.model;
 
+import android.content.ContentValues;
 import android.content.Context;
-import android.content.res.Resources;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
-import com.example.alexparpas.wsjf.R;
 import com.example.alexparpas.wsjf.database.JobBaseHelper;
+import com.example.alexparpas.wsjf.database.JobCursorWrapper;
+import com.example.alexparpas.wsjf.database.JobDbSchema;
+import com.example.alexparpas.wsjf.database.JobDbSchema.JobTable;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.UUID;
 
 /**
@@ -18,16 +19,13 @@ import java.util.UUID;
  */
 public class JobLab {
     private static JobLab sJobLab;
-    private List<Job> mJobs;
     private Context mContext;
     private SQLiteDatabase mDatabase;
 
     private JobLab(Context context) {
         mContext = context.getApplicationContext();
         mDatabase = new JobBaseHelper(mContext).getWritableDatabase();
-        mJobs = new ArrayList<>();
     }
-
 
     public static JobLab get(Context context) {
         if (sJobLab == null) {
@@ -36,20 +34,74 @@ public class JobLab {
         return sJobLab;
     }
 
-    public void addJob(Job j){
-        mJobs.add(j);
+    public void addJob(Job j) {
+        ContentValues values = getContentValues(j);
+        mDatabase.insert(JobTable.NAME, null, values);
     }
 
     public List<Job> getJobs() {
-        return mJobs;
+        List<Job> jobs = new ArrayList<>();
+        JobCursorWrapper cursor = queryCrimes(null, null);
+
+        try {
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                jobs.add(cursor.getJob());
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.close();
+        }
+        return jobs;
     }
 
-    public Job getJob(UUID id){
-        for(Job job: mJobs){
-            if(job.getId().equals(id)){
-                return job;
+    public Job getJob(UUID id) {
+        JobCursorWrapper cursor = queryCrimes(JobTable.Cols.UUID + " = ?", new String[]{id.toString()});
+
+        try {
+            if (cursor.getCount() == 0) {
+                return null;
             }
+            cursor.moveToFirst();
+            return cursor.getJob();
+        } finally {
+            cursor.close();
         }
-        return null;
+    }
+
+    public void updateJob(Job job) {
+        String uuidString = job.getId().toString();
+        ContentValues values = getContentValues(job);
+
+        mDatabase.update(JobTable.NAME, values, JobTable.Cols.UUID + " = ?", new String[]{uuidString});
+    }
+
+    private JobCursorWrapper queryCrimes(String whereClause, String[] whereArgs) {
+        Cursor cursor = mDatabase.query(
+                JobTable.NAME,
+                null,
+                whereClause,
+                whereArgs,
+                null,
+                null,
+                null
+        );
+        return new JobCursorWrapper(cursor);
+    }
+
+    private static ContentValues getContentValues(Job job) {
+        ContentValues values = new ContentValues();
+        values.put(JobTable.Cols.UUID, job.getId().toString());
+        values.put(JobTable.Cols.JOB_NAME, job.getJobName());
+        values.put(JobTable.Cols.DESCRIPTION, job.getJobDescription());
+        values.put(JobTable.Cols.USER_VALUE, job.getUserValue());
+        values.put(JobTable.Cols.TIME_VALUE, job.getTimeValue());
+        values.put(JobTable.Cols.RROE_VALUE, job.getRroeValue());
+        values.put(JobTable.Cols.JOB_SIZE, job.getJobSize());
+        values.put(JobTable.Cols.WSJF_VALUE, job.getWsjfScore());
+        values.put(JobTable.Cols.DATE, job.getDate().getTime());
+        values.put(JobTable.Cols.COMPLETED, job.isCompleted() ? 1 : 0);
+
+        return values;
     }
 }
